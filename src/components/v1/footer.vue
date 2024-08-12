b1175deef60ed3379bdf9063c470dc7ce4072db0
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-08-12 23:49:32 +0800

    :sparkles: 新增 `v1/top/info`

---
+++ src/components/v1/footer.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="text-center my-5">
+		<span class="font-template text-center text-5 fw-900 leading-[100%] text-[#71717b]">POWERED BY</span>
+
+		<br/>
+
+		<span class="font-template text-center text-5 fw-500 leading-[100%] text-[#71717b]">NoneBot2 × nonebot-plugin-tetris-stats</span>
+
+		<br/>
+
+		<div class="mt-1.5">
+			<span class="font-template text-center text-5 fw-500 leading-[100%] text-[#71717b]">
+				<span>Graphic Design by</span>
+				<span>&nbsp;</span>
+
+				<span>C</span>
+				<span class="text-3 align-sub">29</span>
+				<span>H</span>
+				<span class="text-3 align-sub">25</span>
+				<span>N</span>
+				<span class="text-3 align-sub">3</span>
+				<span>O</span>
+				<span class="text-3 align-sub">5</span>
+			</span>
+		</div>
+	</div>
+</template>
+<script lang="ts" setup>
+</script>
---
+++ src/pages/v1/top/info/test.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import type { VNode } from 'vue'
+import Languages from '~/core/shared/languages'
+import V1TopInfo from '~/pages/v1/top/info/index.vue'
+
+const pages = ref<VNode[]>([])
+
+onMounted(async () => {
+	for (const lang of [Languages.enUS, Languages.zhCN]) {
+		document.querySelector('template#data')!.innerHTML = JSON.stringify({
+			_lang: lang,
+			user: {
+				avatar: 'https://tetr.io/user-content/avatars/5eb270aaeb7d4250d3f2fc47.jpg',
+				name: 'SCDHH'
+			},
+			today: {
+				lpm: 48.72,
+				pps: 2.03,
+				apm: 76.87,
+				apl: 1.58
+			},
+			historical: {
+				lpm: 48.72,
+				pps: 2.03,
+				apm: 76.87,
+				apl: 1.58
+			}
+		})
+
+		await nextTick()
+
+		const rendered = h(V1TopInfo)
+
+		pages.value.push(rendered)
+
+		await nextTick()
+	}
+})
+</script>
+
+<template>
+	<div class="flex flex-col p-2 gap-2">
+		<template v-for="page in pages">
+			<Component :is="page"/>
+		</template>
+	</div>
+</template>
