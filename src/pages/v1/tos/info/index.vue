214c436e796f4f62d46bfcb378b43843c6ada688
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-08-14 23:26:43 +0800

    :sparkles: 新增 `v1/tos/info`

---
+++ src/pages/v1/tos/info/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="w-fit bg-[#f1f1f1] px-6.25">
+		<div class="mt-5.5">
+			<v1-tos-info-account_rankings/>
+		</div>
+
+		<div class="mt-10.75">
+			<v1-tos-info-multiplayer-stats/>
+		</div>
+
+		<div class="mt-10.75">
+			<v1-tos-info-singleplayer-stats/>
+		</div>
+
+		<v1-footer/>
+	</div>
+</template>
+
+<style lang="scss">
+@import '~/styles/v1';
+</style>
---
+++ src/pages/v1/tos/info/test.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+import type { VNode } from 'vue'
+import Languages from '~/core/shared/languages'
+import V1TosInfo from '~/pages/v1/tos/info/index.vue'
+
+const pages = ref<VNode[]>([])
+
+onMounted(async () => {
+	for (const lang of [Languages.enUS, Languages.zhCN]) {
+		document.querySelector('template#data')!.innerHTML = JSON.stringify({
+			_lang: lang,
+			user: {
+				avatar: 'https://tetr.io/user-content/avatars/5eb270aaeb7d4250d3f2fc47.jpg',
+				name: 'SCDHH'
+			},
+			multiplayer: {
+				lpm: 48.72,
+				pps: 2.03,
+				apm: 76.87,
+				apl: 1.58,
+				adpm: 102.31,
+				vs: 170.51,
+				adpl: 2.1,
+				app: 0.63,
+				ci: 37.15,
+				dspp: 0.20,
+				or: 0.42,
+				ge: 0.26
+			},
+			singleplayer: {
+				'40l': '1m 10.2s',
+				challenge: '289,085',
+				marathon: '289,085'
+			}
+		})
+
+		await nextTick()
+
+		const rendered = h(V1TosInfo)
+
+		pages.value.push(rendered)
+
+		await nextTick()
+	}
+})
+</script>
+
+<template>
+	<div class="flex flex-col p-2 gap-2">
+		<template v-for="page in pages">
+			<Component :is="page"/>
+		</template>
+	</div>
+</template>
