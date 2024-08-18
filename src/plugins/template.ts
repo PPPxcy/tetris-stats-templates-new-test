62b637680a3ae99d260243266d43c0931d1e87af
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-08-18 14:00:37 +0800

    :sparkles: 处理 `template#path`

---
+++ src/plugins/template.ts
@@ -0,0 +1,9 @@
+export default defineNuxtPlugin(app => {
+	app.hooks.addHooks({
+		'app:created': () => {
+			navigateTo({
+				path: document.querySelector('template#path')!.innerHTML.trim()
+			})
+		}
+	})
+})
