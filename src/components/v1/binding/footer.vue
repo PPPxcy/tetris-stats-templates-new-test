dc6da58af10a3c58a84076d9f8c795350661422c
shoucandanghehe <wallfjjd@gmail.com>
2025-04-10 04:29:45 +0800

    ✨ 简化逻辑

--- src/components/v1/binding/footer.vue
+++ src/components/v1/binding/footer.vue
@@ -8,19 +8,20 @@ const data = useData(
     z
         .union([
             z.object({
-                type: z.union([z.literal(Status.SUCCESS), z.literal(Status.UNVERIFIED), z.literal(Status.UNKNOWN)]),
-                command: z.string(),
+                type: z.union([
+                    z.literal(Status.SUCCESS),
+                    z.literal(Status.UNVERIFIED),
+                    z.literal(Status.UNKNOWN),
+                    z.literal(Status.ERROR),
+                ]),
+                prompt: z.string(),
             }),
             z.object({
                 type: z.literal(Status.UNBIND),
-                command: z.string(),
+                prompt: z.string(),
                 bot: z.object({
                     name: z.string(),
                 }),
-            }),
-            z.object({
-                type: z.literal(Status.ERROR),
-                error: z.string(),
             }),
         ])
         .readonly(),
@@ -34,22 +35,11 @@ const data = useData(
         tag="span"
         class="w-81 mx-auto font-template text-4 text-center text-[#52525C]"
     >
-        <template
-            v-if="
-                data.type === Status.SUCCESS ||
-                data.type === Status.UNVERIFIED ||
-                data.type === Status.UNKNOWN ||
-                data.type === Status.UNBIND
-            "
-            v-slot:command
-        >
-            <span class="fw-500">“{{ data.command }}”</span>
+        <template v-slot:prompt>
+            <span class="fw-500">“{{ data.prompt }}”</span>
         </template>
         <template v-if="data.type === Status.UNBIND" v-slot:botName>
             <span class="fw-500">{{ data.bot.name }}</span>
-        </template>
-        <template v-if="data.type === Status.ERROR" v-slot:error>
-            <span>{{ data.error }}</span>
         </template>
     </i18n-t>
 </template>
